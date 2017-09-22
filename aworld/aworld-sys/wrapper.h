9ffe4a22e400cd9aec294b0d78399e9a803737c6
Sgeo <sgeoster@gmail.com>
2017-09-21 23:27:43 -0400

    * Search for aw64.dll properly. * wrapper.h (probably unnecessary, followed buildgen tutorial too closely) * Working aw_init call.

---
+++ aworld/aworld-sys/wrapper.h
@@ -0,0 +1,1 @@
+#include "Aw.h"
--- aworld/src/lib.rs
+++ aworld/src/lib.rs
@@ -1,6 +1,21 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
+use std::os::raw::c_int;
+extern crate aworld_sys;
+
+fn rc(rc: c_int) -> Result<(), c_int> {
+    if rc == 0 {
+        Ok(())
+    } else {
+        Err(rc)
     }
+}
+
+pub fn init() -> Result<(), c_int> {
+    rc(unsafe {
+        aworld_sys::aw_init(101)
+    })
+}
+
+#[test]
+fn testinit() {
+    init().expect("Unable to init");
 }
